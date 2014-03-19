30b40d57fad1c6a4a12cce20e9a4a1af7f2a7fc0
namiheike <namiheike@gmail.com>
2014-03-19 10:11:28 +0800

    config assets compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,16 @@ Sf2014::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+  # TODO this should be false, it's true now since i'm just using webrick
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  # TODO should be false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
